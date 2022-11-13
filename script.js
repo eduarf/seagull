a6423e2a77de90cfbdfcb69204aa87a6067c33e5
eduarf <fraude_f@yahoo.com>
2022-11-14 01:21:25 +0300

    new nav scrolling

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const navButton = document.querySelector('.navigation__button');
 const navBottom = document.querySelector('.nav-bottom');
-const sec_info = document.querySelector('.sec-info');
-const sec_products = document.querySelector('.sec-products');
-const sec_counter = document.querySelector('.sec-counter');
-const sec_card = document.querySelector('.sec-card');
-const sec_download = document.querySelector('.sec-download');
-const sec_looking_for = document.querySelector('.sec-looking-for');
-const sec_form = document.querySelector('.sec-form');
-const nav_home = document.getElementById('home');
-const nav_about = document.getElementById('about');
-const nav_products = document.getElementById('products');
-const nav_references = document.getElementById('references');
-const nav_hire = document.getElementById('hire');
-const nav_contact = document.getElementById('contact');
-const top_button = document.getElementById('top-button');
+
 
 window.onresize = reportWindowSize;
 
@@ -33,27 +20,10 @@ function reportWindowSize() {
   }
 }
 
-nav_products.addEventListener('click',function(e){
-  e.preventDefault();
-  sec_card.scrollIntoView({behavior: 'smooth'});
-});
-
-nav_references.addEventListener('click',function(e){
-  e.preventDefault();
-  sec_counter.scrollIntoView({behavior: 'smooth'});
-});
-
-nav_contact.addEventListener('click',function(e){
-  e.preventDefault();
-  sec_form.scrollIntoView({behavior: 'smooth'});
-});
-
-top_button.addEventListener('click', function(e){
-  e.preventDefault();
-  sec_products.scrollIntoView({behavior: 'smooth'});
-});
-
-nav_hire.addEventListener('click', function(e){
-  e.preventDefault();
-  sec_looking_for.scrollIntoView({behavior: 'smooth'});
+document.querySelectorAll('.nav-top__link').forEach(function(el){
+  el.addEventListener('click',function(e){
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(`${id}`).scrollIntoView({behavior: 'smooth'});
+  });
 });
